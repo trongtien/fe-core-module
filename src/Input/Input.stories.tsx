a68cd8548dfb54e936a8e607f106144ad4f5075e
tienplt <tienplt@scb.com.vn>
2022-06-05 23:02:41 +0700

    Update Input core service

---
+++ src/Input/Input.stories.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Input from "./Input";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "ReactComponentLibrary/Input",
+  component: Input,
+} as ComponentMeta<typeof Input>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const InputDefault = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+InputDefault.args = {
+    label: "Hello world!",
+    value: "outlined"
+};
+
+
+export const InputValidateError = Template.bind({});
+
+InputValidateError.args = {
+    label: "Hello world!",
+    value: "outlined",
+    message:"Message error"
+};
--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React, { FC, HTMLInputTypeAttribute } from 'react';
+import React, { FC, HTMLInputTypeAttribute, useEffect, useState, memo, useRef, ChangeEvent } from 'react';
 import clsx from 'clsx';
 import TextField from "@mui/material/TextField";
 
@@ -9,31 +9,76 @@ export interface InputProps{
     label?: string;
     type?: HTMLInputTypeAttribute;
     value?: string | number;
-    onChange?(): void;
+    onChangeTimeOut?(val: string): void;
+    onChange?(val: string): void;
 }
 
+interface IObjectInput{
+    valueData?: string | number;
+    messageError?: string;
+}
 
 const Input: FC<InputProps> = props => {
 
-    const { className, id, message, label, type, value, onChange} = props;
+    const { className, id, message = '', label, type='text', value, onChangeTimeOut, onChange } = props;
 
-    const inputClassName = clsx("core-input", id ? `core-input-${id}` : "", className)
+    const timeOutCurrent = 500;
+    const inputClassName = clsx("core-input", id ? `core-input-${id}` : "", className);
+    
+    const timer = useRef<ReturnType<typeof setTimeout>>();
 
-    const onHandleChange= () => {
-        onChange && onChange()
+    const [ objectDefaultData, setObjectDefaultData] = useState<IObjectInput>({
+        messageError: message,
+        valueData: value
+    })
+
+    useEffect(() => {
+        if(message.length === 0){
+            setObjectDefaultData({
+                ...objectDefaultData,
+                messageError: ""
+            })
+        }
+        else if(message !== objectDefaultData.messageError){
+            setObjectDefaultData({
+                ...objectDefaultData,
+                messageError: message ?? ""
+            })
+        }
+
+        if(objectDefaultData.valueData !== value){
+            setObjectDefaultData({
+                ...objectDefaultData,
+                valueData: value
+            })
+            timer.current && clearTimeout(timer.current);
+            timer.current = setTimeout(() => {
+                onChangeTimeOut && onChangeTimeOut(value?.toString()??'');
+            }, timeOutCurrent);
+        }
+    }, [value, message])
+
+    const onHandleChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+        let val = event.target.value;
+        if(type === 'number' && isNaN(+val)){
+            val = "0"
+        }
+
+        onChange && onChange(val.toString());
     }
 
     return (
         <TextField 
             id={id}
+            error={(objectDefaultData?.messageError ?? "")?.length > 0 ? true : false }
             className={inputClassName}
-            helperText={message}
+            helperText={objectDefaultData?.messageError ?? ''}
             label={label}
             type={type}
-            value={value}
-            onChange={onHandleChange}
+            value={objectDefaultData.valueData}
+            onChange={onHandleChangeInput}
         />
     )
 }
 
-export default Input;
+export default memo(Input);
